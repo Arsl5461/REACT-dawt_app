a4925b372bad880993330cd2d12f7c669c246f6b
Imran Shahzad <imranshahzad786@gmail.com>
2023-06-19 11:52:23 +0500

    Routing added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import 'swiper/css';
 import Slider from "./components/Slider"
 import Counter from "./components/Counter";
 import User from "./components/User";
+import Home from "./components/Home";
+import {Routes,Route} from "react-router-dom"
+import Contact from "./components/Contact";
+import SignUp from "./components/SignUp";
+import AboutUs from "./components/AboutUs";
 function App() {
   return (
     <>
   <Navbar/>
-  <Slider/>
-  <Counter/>
-  <User/>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+  <Route path="/products" element={<User/>}></Route>
+  <Route path="/contact" element={<Contact/>}></Route>
+  <Route path="/about" element={<AboutUs/>}></Route>
+  <Route path="/signup" element={<SignUp/>}></Route>
+
+  </Routes>
+
   </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 function Navbar() {
   return(
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <Link className="navbar-brand" to="/">Navbar</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <Link className="nav-link" to="/products">Products</Link>
         </li>
         <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -28,7 +28,13 @@ function Navbar() {
           </ul>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <Link className="nav-link" to="/contact" tabindex="-1" aria-disabled="true">Contact Us</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/about" tabindex="-1" aria-disabled="true">About Us</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/signup" tabindex="-1" aria-disabled="true">Sign Up</Link>
         </li>
       </ul>
       <form className="d-flex">
