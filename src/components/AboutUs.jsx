06e2e9a73a26baadb06bf94831d362869ce529e6
Imran Shahzad <imranshahzad786@gmail.com>
2023-06-20 12:03:07 +0500

    Fetch Api Done

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import "./About.css"
+import {useState,useEffect} from "react"
 
 function AboutUs() {
+const [userData,setUserData]=useState(null);
+
+const fetchData=async()=>{
+try{
+const response=await fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=political&type=single")
+const data=await response.json();
+setUserData(data);
+}
+catch(err){
+  console.log(err)
+}
+}
+useEffect(()=>{
+  fetchData();
+},[])
   return (
-    <div>AboutUs</div>
+<h1>{userData?.joke}</h1>  
   )
 }
 
